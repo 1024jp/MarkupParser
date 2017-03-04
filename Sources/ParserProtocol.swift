94125aa5ef0f31d84c37578cf8da5e8064d49ceb
1024jp <1024jp@wolfrosch.com>
2017-03-04 15:30:08 +0900

    Implement `parse(string)` method

--- Sources/ParserProtocol.swift
+++ Sources/ParserProtocol.swift
@@ -35,3 +35,30 @@ protocol Parsable {
 }
 
 
+extension Parsable {
+
+    func parse(string: String) -> Document {
+        
+        let lines = string.components(separatedBy: .newlines)
+        
+        let chunks = lines.enumerated()
+            .flatMap { (index: Int, line: String) -> Chunk? in
+                guard line.isEmpty else { return nil }
+                
+                if let level = self.headingLevel(string: line) {
+                    
+                    return Chunk.heading(string: string, level: level)
+                }
+                
+                return Chunk.nonHeading(string: string)
+        }
+        
+        let section = self.createSection(chunks: chunks)
+        
+        return Document(rawString: string,
+                        langauge: self.langauge,
+                        sectioin: section)
+    }
+    
+
+}
