5b9c41b8216fe4d136f8c8a4df7a053dabf45727
1024jp <1024jp@wolfrosch.com>
2017-03-04 15:17:02 +0900

    Update StyledString structure

--- Sources/StyledString.swift
+++ Sources/StyledString.swift
@@ -10,20 +10,21 @@ struct StyledString {
     
     let string: String
     let styles: [Style]
-}
-
-
-struct Style {
-    
-    let range: Range<String.Index>
-    let type: Type
     
     
-    enum Type {
+    struct Style {
         
-        case bold
-        case strike
-        case link(url: String)
-        case image(url: String)
+        let range: Range<String.Index>
+        let type: Type
+        
+        
+        enum Type {
+            
+            case bold
+            case strike
+            case link(url: String)
+            case image(url: String)
+        }
     }
+    
 }
