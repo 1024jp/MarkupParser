aba3547e8f3384370d32195543e99b303198a54f
1024jp <1024jp@wolfrosch.com>
2017-03-04 17:12:08 +0900

    Impelement document tree parsing

--- Sources/ParserProtocol.swift
+++ Sources/ParserProtocol.swift
@@ -52,5 +52,61 @@ extension Parsable {
                         section: section)
     }
     
+    
+    
+    func createSection(chunks: [Chunk], title: StyledString? = nil, level: Int = 0) -> Section {
+        
+        var section = Section(title: title, level: level)
+        
+        var blockLines = [String]()
+        var lastTitle: String?
+        var childChunks = [Chunk]()
+        let childLevel = level + 1
+        
+        chunkLoop: for chunk in chunks {
+            switch chunk {
+            case .heading(let string, let currentLevel):
+                if currentLevel < childLevel {
+                    childChunks.append(chunk)
+                    continue
+                }
+                
+                if let title = lastTitle {
+                    let styledTitle = self.makeStyledString(string: title)
+                    let childSection = self.createSection(chunks: childChunks,
+                                                          title: styledTitle,
+                                                          level: childLevel)
+                    section.subsections.append(childSection)
+                }
+                
+                guard currentLevel > level else {
+                    break chunkLoop
+                }
+                
+                childChunks = []
+                lastTitle = string
+                
+            case .nonHeading(let string):
+                if childChunks.isEmpty {
+                    blockLines.append(string)
+                } else {
+                    childChunks.append(chunk)
+                }
+            }
+        }
+        
+        
+        if let title = lastTitle {
+            let styledTitle = self.makeStyledString(string: title)
+            let childSection = self.createSection(chunks: childChunks,
+                                                  title: styledTitle,
+                                                  level: childLevel)
+            section.subsections.append(childSection)
+        }
+        
+        section.contents = self.makeBlocks(lines: blockLines)
+        
+        return section
+    }
 
 }
