e1f1c4e02f61bd8683a5ea45e7054128f5391e75
星野恵瑠 <shonetesmith@gmail.com>
2017-03-04 15:26:49 +0900

    Create a framework to manage the files

---
+++ MarkupParser/MarkupParser/MarkupParser.h
@@ -0,0 +1,19 @@
+//
+//  MarkupParser.h
+//  MarkupParser
+//
+//  Created by 史翔新 on 2017/03/04.
+//  Copyright © 2017年 Crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MarkupParser.
+FOUNDATION_EXPORT double MarkupParserVersionNumber;
+
+//! Project version string for MarkupParser.
+FOUNDATION_EXPORT const unsigned char MarkupParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkupParser/PublicHeader.h>
+
+
